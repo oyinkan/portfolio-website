7d0bb7eb2267e21465efc1c4c060c73598664c4d
oyinkan <skusman@student.lautech.edu.ng>
2024-06-23 13:10:58 +0100

    added background image to the about page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -18,38 +18,38 @@ function Header() {
       <section className="name-section">
         <p>Hi,I’m</p>
         <h1>Sakirat Usman.</h1>
-        <p className="profession">Software Engineer | Data Analyst</p>
+        <p className="profession">Software Engineer | Data Scientist</p>
       </section>
       <section className="social-links">
-          <a
-            href="https://www.linkedin.com/in/sakirat-kehinde-usman-3a4600117/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaLinkedin />
-          </a>
-          <a
-            href="https://www.github.com/oyinkan"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaGithub />
-          </a>
-          <a
-            href="https://stackoverflow.com/users/13958137/shakirah"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaStackOverflow />
-          </a>
-          <a
-            href="https://www.twitter.com/oyinkan93"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaTwitter />
-          </a>
-        </section>
+        <a
+          href="https://www.linkedin.com/in/sakirat-kehinde-usman-3a4600117/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://www.github.com/oyinkan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/13958137/shakirah"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaStackOverflow />
+        </a>
+        <a
+          href="https://www.twitter.com/oyinkan93"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTwitter />
+        </a>
+      </section>
     </header>
   );
 }

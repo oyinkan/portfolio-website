99df8ee83644948994407a02ca23985b57b227d8
oyinkan <skusman@student.lautech.edu.ng>
2024-06-30 21:38:24 +0100

    added animations

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,38 +1,42 @@
 import "./About.css";
+import ScrollAnimation from "react-animate-on-scroll";
 
 function About() {
   return (
-    <section className="about-me">
-      <div className="flex">
-        <img src="/logo.svg" className="logo" alt="Sakirat Kehinde Usman" />
-        <hr className="line" />
-      </div>
-      <h3>About Me</h3>
-      <p>
-        I am an engineer with a solid foundation in both frontend engineering
-        and data science. With a Bachelor’s degree in Computer Science and a
-        Master’s in Data Science, I bring a blend of practical programming and
-        analytical expertise to my work. Over three years as a frontend
-        engineer, I’ve honed my skills in crafting user-friendly web
-        applications using JavaScript, React, and CSS, which has enhanced my
-        problem-solving abilities and attention to detail.
-        <br />
-        <br />
-        My journey into data science involved hands-on projects where I applied
-        machine learning, statistical analysis, and data manipulation techniques
-        using Python and libraries like Pandas, NumPy, Scikit-learn, and
-        PyTorch. I have worked on clustering and sentiment analysis projects,
-        deriving actionable insights from large datasets. I am passionate about
-        leveraging my technical skills and continuous learning mindset to solve
-        complex problems and create impactful solutions in a collaborative
-        environment.
-      </p>
-      <h5>SKILLS</h5>
-      <p>
-        HTML . CSS . JavaScript . React . Redux . NodeJS . MongoDB . <br />
-        Python . SQL . Excel . R . Numpy . Panda . 
-      </p>
-    </section>
+    <ScrollAnimation animateIn="fadeIn">
+      <section className="about-me">
+        <div className="flex">
+          <img src="/logo.svg" className="logo" alt="Sakirat Kehinde Usman" />
+          <hr className="line" />
+        </div>
+        <h3>About Me</h3>
+        <p>
+          I am an engineer with a solid foundation in both frontend engineering
+          and data science. With a Bachelor’s degree in Computer Science and a
+          Master’s in Data Science, I bring a blend of practical programming and
+          analytical expertise to my work. Over three years as a frontend
+          engineer, I’ve honed my skills in crafting user-friendly web
+          applications using JavaScript, React, and CSS, which has enhanced my
+          problem-solving abilities and attention to detail.
+          <br />
+          <br />
+          My journey into data science involved hands-on projects where I
+          applied machine learning, statistical analysis, and data manipulation
+          techniques using Python and libraries like Pandas, NumPy,
+          Scikit-learn, and PyTorch. I have worked on clustering and sentiment
+          analysis projects, deriving actionable insights from large datasets. I
+          am passionate about leveraging my technical skills and continuous
+          learning mindset to solve complex problems and create impactful
+          solutions in a collaborative environment.
+        </p>
+        <h5>SKILLS</h5>
+        <p>
+          HTML . CSS . JavaScript . React . Redux . NodeJS . MongoDB <br />
+          Python . SQL . Excel . R . Numpy . Pandas . Scikit-Learn. Tensorflow.
+          Keras
+        </p>
+      </section>
+    </ScrollAnimation>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,5 @@
+import ScrollAnimation from "react-animate-on-scroll";
+import { GoArrowDownRight } from "react-icons/go";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaStackOverflow } from "react-icons/fa";
@@ -15,40 +17,45 @@ function Header() {
           Download Resume
         </a>
       </div>
-      <section className="name-section">
-        <p>Hi,I’m</p>
-        <h1>Sakirat Usman.</h1>
-        <p className="profession">Software Engineer | Data Scientist</p>
-      </section>
+      <ScrollAnimation animateIn="slideInDown">
+        <section className="name-section">
+          <p>Hi,I’m</p>
+          <h1>Sakirat Usman.</h1>
+          <p className="profession">Software Engineer | Data Scientist</p>
+        </section>
+      </ScrollAnimation>
       <section className="social-links">
-        <a
-          href="https://www.linkedin.com/in/sakirat-kehinde-usman-3a4600117/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaLinkedin />
-        </a>
-        <a
-          href="https://www.github.com/oyinkan"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaGithub />
-        </a>
-        <a
-          href="https://stackoverflow.com/users/13958137/shakirah"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaStackOverflow />
-        </a>
-        <a
-          href="https://www.twitter.com/oyinkan93"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaTwitter />
-        </a>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/sakirat-kehinde-usman-3a4600117/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://www.github.com/oyinkan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://stackoverflow.com/users/13958137/shakirah"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaStackOverflow />
+          </a>
+          <a
+            href="https://www.twitter.com/oyinkan93"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaTwitter />
+          </a>
+        </div>
+        <GoArrowDownRight />
       </section>
     </header>
   );
